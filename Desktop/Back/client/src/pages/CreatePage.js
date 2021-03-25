070ead1c6b53f892d383703ae8867f49c308aa7a
iKornev <ik0077ik25@gmail.com>
2021-03-25 23:05:43 +0200

    olol

---
+++ Desktop/Back/client/src/pages/CreatePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const CreatePage = () => {
+return (
+    <div> 
+    {/* <h1>Change your language</h1> */}
+        <div>
+            <div>
+                <a href="https://react-app-da894.firebaseapp.com/">
+                    <img style={{width: '150px',float:'left',marginRight:'20rem'}} src="https://telegram.org.ru/uploads/posts/2017-07/1500748538_cropped-512_pencil-1.png"></img>
+                </a>
+            </div>
+            {/* <div>
+                <a href="https://fav757.github.io/index.html">
+                    <img style={{width: '150px',float:'left'}} src="https://st.depositphotos.com/1141341/2558/i/450/depositphotos_25587499-stock-photo-flag-of-uk.jpg"></img>
+                </a>
+            </div>  */}
+        </div>
+    
+
+    </div>   
+)
+
+}
